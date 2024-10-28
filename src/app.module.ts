6e58c3a8a10ddbba8a876a850de407bb22ae6d48
michelp76 <michel.puissant@inextenso.fr>
2024-10-28 17:05:22 +0100

    dossier "airports" renommé en "airports_data" + cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
-import { AirportsModule } from './airports/airports.module';
+import { AirportsModule } from './airports_data/airports.module';
 import { ConfigModule } from '@nestjs/config';
-import { RoutesModule } from './routes/routes.module';
 @Module({
   imports: [
     DrizzleModule,
     AirportsModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    RoutesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
