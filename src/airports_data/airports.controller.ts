704e51f67f513f2b6b0d3ecae7e75864bacfd6e8
michelp76 <michel.puissant@inextenso.fr>
2024-11-25 15:25:47 +0100

    Logs (console + fichiers) avec Winston (nest-winston)

--- src/airports_data/airports.controller.ts
+++ src/airports_data/airports.controller.ts
@@ -1,34 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+} from '@nestjs/common';
 import { AirportsService } from './airports.service';
 import { CreateAirportDto } from './dto/create-airport.dto';
 import { UpdateAirportDto } from './dto/update-airport.dto';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Controller('airports_data')
 export class AirportsController {
-  constructor(private readonly airportsService: AirportsService) {}
+  constructor(
+    private readonly airportsService: AirportsService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
 
   @Post()
   create(@Body() createAirportDto: CreateAirportDto) {
+    this.logger.log('info', 'Méthode "%s" - Objet: %s', 'create', createAirportDto);
+    // info: test message 123 {}
+    // this.logger.log('info', 'test message %d', 123);    
     return this.airportsService.create(createAirportDto);
   }
 
   @Get()
   findAll() {
+    this.logger.log('info', 'Méthode "%s"', 'findAll');
     return this.airportsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    this.logger.log('info', 'Méthode "%s" - Param: %s', 'findOne', id);
     return this.airportsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAirportDto: UpdateAirportDto) {
+    this.logger.log('info', 'Méthode "%s" - Param: %s - Objet: %s', 'update', id, updateAirportDto);
     return this.airportsService.update(id, updateAirportDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    this.logger.log('info', 'Méthode "%s" - param %s', 'delete', id);
     return this.airportsService.remove(id);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,53 @@ import { AppService } from './app.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { AirportsModule } from './airports_data/airports.module';
 import { ConfigModule } from '@nestjs/config';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 @Module({
   imports: [
     DrizzleModule,
     AirportsModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      format: winston.format.combine(
+        // winston.format.label({ label: '[my-label]' }),
+        winston.format.splat(),
+        winston.format.json(),
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.printf(
+          (info) => `${info.timestamp} | ${info.level} | ${info.message}`,
+        ),
+      ),
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('NestApp', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+        //
+        // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+        // - Write all logs error (and below) to `quick-start-error.log`.
+        //
+        new winston.transports.File({
+          filename: 'quick-start-combined.log',
+        }),
+        new winston.transports.File({
+          filename: 'quick-start-error.log',
+          level: 'error',
+        }),
+      ],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
