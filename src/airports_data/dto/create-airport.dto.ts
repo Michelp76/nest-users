b18d90d45667ddff91cf9c554924d88004e6a13f
michelp76 <michel.puissant@inextenso.fr>
2024-12-09 15:57:33 +0100

    Class validators pour contrôler les saisies

--- src/airports_data/dto/create-airport.dto.ts
+++ src/airports_data/dto/create-airport.dto.ts
@@ -1,10 +1,26 @@
+import { IsString, IsNotEmpty, MinLength, MaxLength, Contains } from 'class-validator';
+
 export class CreateAirportDto {
+  @IsNotEmpty()
+  @IsString()    
+  @MinLength(3)
+  @MaxLength(3)
   airportCode: string;
+
+  @IsNotEmpty()
+  @Contains('Airport')
   airportName: string;
+
+  @IsNotEmpty()
   city: string;
+
+  @IsNotEmpty()
+  // @IsLatLong()
   coordinates: {
     x: number;
     y: number;
   };
+
+  @IsNotEmpty()
   timezone: string;
 }
--- src/flights/dto/create-flight.dto.ts
+++ src/flights/dto/create-flight.dto.ts
@@ -1,12 +1,38 @@
+import { IsDate, IsDateString, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateFlightDto {
+  @IsNotEmpty()
   flightId: number;
+
+  @IsNotEmpty()
   flightNo: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   scheduledDeparture: string;
+
+  @IsNotEmpty()
+  @IsDateString()    
   scheduledArrival: string;
+
+  @IsNotEmpty()
   departureAirport: string;
+
+  @IsNotEmpty()
   arrivalAirport: string;
+
+  @IsNotEmpty()
   status: string;
+
+  @IsNotEmpty()  
+  @IsString()
+  @MinLength(3)
+  @MaxLength(3)
   aircraftCode: string;
+
+  // @IsDateString()  
   actualDeparture: string;
+
+  // @IsDateString()  
   actualArrival: string;
 }
